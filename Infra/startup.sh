a08ff3d808f562bbb1a87125038f9001980e3bc7
Gustavo Sana <gustavosana1@gmail.com>
2025-07-09 19:36:52 -0300

    Alteração do Startup.sh

--- Infra/startup.sh
+++ Infra/startup.sh
@@ -1,8 +1,26 @@
-apt update 
-apt install -y docker.io git
+#!/bin/bash
 
-git clone https://github.com/Satamoita/quiz-react-real.git/quizbdf
-cd /quizbdf
+# Atualiza pacotes
+yum update -y
 
+# Instala Docker e Git
+yum install -y docker git
+
+# Inicia o Docker
+service docker start
+usermod -aG docker ec2-user
+
+# Vai para a pasta do usuário padrão
+cd /home/ec2-user
+
+# Clona o repositório
+git clone https://github.com/Satamoita/QuizDev.git
+
+# Entra no diretório do app
+cd QuizDev
+
+# Constrói a imagem Docker com o nome desejado
 docker build -t quiz-react-real .
-docker run -d --name quiz-react -p 4173:4173 -e PORT=4173 quiz-react-real
+
+# Sobe o container na porta 80 (externa) → 4173 (interna)
+docker run -d --name quiz-react -p 80:4173 -e PORT=4173 quiz-react-real
